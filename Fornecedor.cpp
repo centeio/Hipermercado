9b83f667cdffe9774b8aab8191cd55b80cce2d89
Tiago <tiagoalmeida.95@hotmail.com>
2015-11-02 21:22:13 +0000

    Fornecedor class implemented

--- Fornecedor.cpp
+++ Fornecedor.cpp
@@ -7,12 +7,41 @@
 
 #include "Fornecedor.h"
 
-Fornecedor::Fornecedor() {
+template<typename T>
+Fornecedor<T>::Fornecedor() {
 	// TODO Auto-generated constructor stub
 
 }
 
-Fornecedor::~Fornecedor() {
+template<typename T>
+Fornecedor<T>::~Fornecedor() {
 	// TODO Auto-generated destructor stub
 }
 
+template<typename T>
+string Fornecedor<T>::getNome() const { return nome; }
+
+template<typename T>
+string Fornecedor<T>::getNIF() const { return NIF; }
+
+template<typename T>
+string Fornecedor<T>::getMorada() const { return morada; }
+
+template<typename T>
+void Fornecedor<T>::addProduto(Produto* produto, T stock, vector<Patamar<T>* > patamares) {
+	ProdFornecedor<T>* prodFornecedor = new ProdFornecedor<T>(produto, stock, patamares);
+
+	produtos.push_back(prodFornecedor);
+}
+
+template<typename T>
+void Fornecedor<T>::decStock(Produto* produto, T quantidade) {
+
+	for(unsigned int i = 0; i < produtos.size(); i++) {
+		if(*produto == *produtos.at(i)->getProduto())
+			produtos.at(i)->setStock(produtos.at(i)->getStock() - quantidade);
+	}
+}
+
+template class Fornecedor<double>;
+template class Fornecedor<int>;
--- Fornecedor.h
+++ Fornecedor.h
@@ -7,11 +7,24 @@
 
 #ifndef FORNECEDOR_H_
 #define FORNECEDOR_H_
+#include "ProdFornecedor.h"
 
+template<typename T>
 class Fornecedor {
+private:
+	string nome, NIF, morada;
+	vector<ProdFornecedor<T>*> produtos;
 public:
 	Fornecedor();
+	Fornecedor(string nome, string NIF, string morada) : nome(nome), NIF(NIF), morada(morada) {};
+	string getNome() const;
+	string getNIF() const;
+	string getMorada() const;
+	void addProduto(Produto* produto, T stock, vector<Patamar<T>* > patamares);
+	void decStock(Produto* produto, T quantidade);
 	virtual ~Fornecedor();
 };
+
+
 
 #endif /* FORNECEDOR_H_ */
