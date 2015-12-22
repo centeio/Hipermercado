072629c4d822e205e42336906953224e1d035ee5
Tiago <tiagoalmeida.95@hotmail.com>
2015-12-22 15:49:40 +0000

    -

--- Fornecedor.h
+++ Fornecedor.h
@@ -36,6 +36,7 @@ class FornecedorIndividual : public Fornecedor {
 protected:
 	string tipo = "Individual";
 public:
+	FornecedorIndividual(string nome, string NIF, string morada) : Fornecedor(nome,NIF,morada) {};
 	FornecedorIndividual(string nome, string NIF, string morada, Data data) : Fornecedor(nome,NIF,morada, data) {};
 	string getTipo() const;
 
@@ -45,6 +46,7 @@ class FornecedorEmpresa : public Fornecedor {
 protected:
 	string tipo = "Empresa";
 public:
+	FornecedorEmpresa(string nome, string NIF, string morada) : Fornecedor(nome,NIF,morada) {};
 	FornecedorEmpresa(string nome, string NIF, string morada, Data data) : Fornecedor(nome,NIF,morada, data) {};
 	string getTipo() const;
 };
